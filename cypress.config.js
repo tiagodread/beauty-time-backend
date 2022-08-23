81a3a6459db37738403ce855b686fd7048f525cf
Tiago Góes <tiago.goes2009@gmail.com>
2022-08-22 22:18:21 -0300

    #5 initial setup to run tests

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+    supportFile: './src/tests/cypress/support/e2e.js',
+    downloadsFolder:'./src/tests/cypress/downloads',
+    fixturesFolder:'./src/tests/cypress/fixtures',
+    screenshotsFolder: './src/tests/cypress/screenshots',
+    videosFolder: './src/tests/cypress/videos',
+    specPattern: './src/tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    viewportHeight: 1080,
+    viewportWidth: 1920
+  },
+});
