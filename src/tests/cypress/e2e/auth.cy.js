bc03a9c73ceae9255888a4edc37d0e4a30fdf885
Tiago Góes <tiago.goes2009@gmail.com>
2022-08-24 21:05:25 -0300

    Add test coverage

--- src/tests/cypress/e2e/auth.cy.js
+++ src/tests/cypress/e2e/auth.cy.js
@@ -1,21 +1,19 @@
-import { faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
 
-describe('Invalid Registration', () => {
-    let body = {}
+describe('/register', () => {
+    let name, email, password = undefined
     beforeEach(() => {
-        body = {
-            name: faker.name.fullName(),
-            email: faker.internet.email(),
-            password: faker.internet.password(20, true)
-        }
+        name = faker.name.fullName()
+        email = faker.internet.email().toLowerCase()
+        password = faker.internet.password(20, true)
     })
 
     it('Should return 400 when name is not specified', () => {
-        body.name = undefined
+        name = undefined
         cy.request({
             method: 'POST',
             url: '/auth/register',
-            body: body,
+            body: {name, email, password},
             failOnStatusCode: false
         }).then((xhr) => {
             expect(xhr.status).to.eq(400)
@@ -25,11 +23,11 @@ describe('Invalid Registration', () => {
     })
 
     it('Should return 400 when email is not specified', () => {
-        body.email = undefined
+        email = undefined
         cy.request({
             method: 'POST',
             url: '/auth/register',
-            body: body,
+            body: {name, email, password},
             failOnStatusCode: false
         }).then((xhr) => {
             expect(xhr.status).to.eq(400)
@@ -39,11 +37,11 @@ describe('Invalid Registration', () => {
     })
 
     it('Should return 400 when password is not specified', () => {
-        body.password = undefined
+        password = undefined
         cy.request({
             method: 'POST',
             url: '/auth/register',
-            body: body,
+            body: {name, email, password},
             failOnStatusCode: false
         }).then((xhr) => {
             expect(xhr.status).to.eq(400)
@@ -51,42 +49,50 @@ describe('Invalid Registration', () => {
             expect(xhr.body.error.name).to.eq('ValidationError')
         })
     })
-})
 
-describe('Valid Registration', () => {
-    it('Should be able to register successfully once', () => {
-        const body = {
-            name: faker.name.fullName(),
-            email: faker.internet.email(),
-            password: faker.internet.password(20, true)
-        }
-        cy.request({
-            method: 'POST',
-            url: '/auth/register',
-            body: body,
-            failOnStatusCode: false
-        }).then((xhr) => {
-            expect(xhr.status).to.eq(200)
-            expect(xhr.body).to.have.property('token')
-            expect(xhr.body).to.have.property('user')
-            expect(xhr.body.user).to.have.property('email')
-            expect(xhr.body.user).to.have.property('name')
-            expect(xhr.body.token).to.not.be.empty
-            expect(xhr.body.user.name).to.not.be.empty
-            expect(xhr.body.user.email).to.not.be.empty
-            expect(xhr.body.user.createdAt).to.not.be.empty
-        })
+    it('Should be able to register successfully just once', () => {
+        cy.createAccount(name, email, password)
         // Make sure duplicate users cannot register
         cy.request({
             method: 'POST',
             url: '/auth/register',
-            body: body,
+            body: {name, email, password},
             failOnStatusCode: false
         }).then((xhr) => {
             expect(xhr.status).to.eq(400)
             expect(xhr.statusText).to.eq('Bad Request')
             expect(xhr.body).to.eql({error: 'User already exists'})
         })
+    })
+})
+
+describe('/authenticate', () => {
+    let name, email, password = undefined
+    beforeEach(() => {
+        name = faker.name.fullName()
+        email = faker.internet.email().toLowerCase()
+        password = faker.internet.password(20, true)
+    })
+
+    it('Can authenticate successfully', () => {
+        cy.createAccount(name, email, password)
+        cy.request({
+            method: 'POST',
+            url: '/auth/authenticate',
+            body: {email, password},
+            failOnStatusCode: false
+        }).then((xhr) => {
+            expect(xhr.status).to.eq(200)
+            expect(xhr.body).to.have.property('token')
+            expect(xhr.body.user.name).to.eq(name)
+            expect(xhr.body.user.email).to.eq(email)
+            expect(xhr.body.user.createdAt).to.not.be.empty
+            expect(xhr.body.user._id).to.not.be.empty
+        })
+    })
+
+    it.skip('Can not authenticate with bad credentials', () => {
+        cy.createAccount(name, email, password)
 
     })
 })
--- src/tests/cypress/support/commands.js
+++ src/tests/cypress/support/commands.js
@@ -23,3 +23,29 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import {faker} from "@faker-js/faker";
+
+Cypress.Commands.add('createAccount', (name, email, password) => {
+    const body = {
+        name: name != undefined ? name : faker.name.fullName(),
+        email: email != undefined ? email.toLowerCase() : faker.internet.email().toLowerCase(),
+        password: password != undefined ? password : faker.internet.password(20, true)
+    }
+    cy.request({
+        method: 'POST',
+        url: '/auth/register',
+        body: body,
+        failOnStatusCode: false
+    }).then((xhr) => {
+        expect(xhr.status).to.eq(200)
+        expect(xhr.body).to.have.property('token')
+        expect(xhr.body).to.have.property('user')
+        expect(xhr.body.user).to.have.property('email')
+        expect(xhr.body.user).to.have.property('name')
+        expect(xhr.body.token).to.not.be.empty
+        expect(xhr.body.user.name).to.not.be.empty
+        expect(xhr.body.user.email).to.not.be.empty
+        expect(xhr.body.user.createdAt).to.not.be.empty
+    })
+})
